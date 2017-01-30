91e50b3ae9fbfa0810fc6c15643c3d54ca6384c0
Mika Yeap <mikaomarr@gmail.com>
2017-01-30 22:16:25 +0800

    Added router and populated (untested); reorganized files

--- public/src/app.ts
+++ public/src/app.ts
@@ -1,3 +1,12 @@
+import {RouterConfiguration, Router} from 'aurelia-router';
+
 export class App {
-  message = 'Hello World!';
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    config.title = 'RAMEN';
+    config.map([
+      { route: ['', 'home'], name: 'home',     moduleId: 'components/home/home',         title:'Home' },
+      { route: 'projects',   name: 'projects', moduleId: 'components/projects/projects', title:'Home' },
+    ]);
+    this.router = router;
+  }
 }
