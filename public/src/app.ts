3601b258bc54988d48917c346b0fa6f428d1db15
Mika Yeap <mikaomarr@gmail.com>
2017-02-17 22:46:12 +0800

    Added Projects route; Fixed leaking CSS styles; Cleaned up overall

--- public/src/app.ts
+++ public/src/app.ts
@@ -7,7 +7,9 @@ export class App {
   configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'RAMEN';
     config.map([
-      { route: ['', 'home'], name: 'home',      moduleId: './home',      nav: true, title: 'Home' }
+      { route: ['', 'home'], name: 'home',      moduleId: './home',       nav: true, title: 'Home' },
+	  { route: 'story',      name: 'our-story', moduleId: './story',      nav: true, title: 'Story' },
+	  { route: 'projects',      name: 'projects', moduleId: './projects', nav: true, title: 'Projects' }
     ]);
 
     this.router = router;
